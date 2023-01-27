30b1ae51de2b007c8bce06f1d382cb1d4865e95f
QS <2725432838@qq.com>
2023-01-27 19:38:48 +0800

    已完成excel导入导出

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,16 +1,169 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <page-tools :show-before="true">
+        <span slot="before">共{{ page.total }}条记录</span>
+        <template slot="after">
+          <el-button size="small" type="success" @click="$router.push('/import?type=user')">excel导入</el-button>
+          <el-button size="small" type="danger" @click="exportData">excel导出</el-button>
+          <el-button size="small" type="primary" @click="showDialog = true">新增员工</el-button>
+        </template>
+      </page-tools>
+      <!-- 放置表格和分页 -->
+      <el-card>
+        <el-table v-loading="loading" :data="list" border>
+          <el-table-column type="index" label="序号" sortable="" />
+          <el-table-column label="姓名" prop="username" sortable="" />
+          <el-table-column label="工号" prop="workNumber" sortable="" />
+          <el-table-column label="聘用形式" prop="formOfEmployment" sortable="" :formatter="formatEmployment" />
+          <el-table-column label="部门" prop="departmentName" sortable="" />
+          <el-table-column label="入职时间" prop="timeOfEntry" sortable="" align="center">
+            <!-- 作用域插槽 -->
+            <template slot-scope="{ row }">
+              {{ row.timeOfEntry | formatDate }}
+            </template>
+          </el-table-column>
+          <el-table-column label="账户状态" prop="enableState" sortable="" align="center">
+            <template slot-scope="{ row }">
+              <!-- 根据当前状态来确定 是否打开开关 -->
+              <el-switch :value="row.enableState === 1" />
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template slot-scope="{ row }">
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small" @click="delEmployee(row.id)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <el-row type="flex" justify="center" align="middle" style="height: 60px">
+          <el-pagination
+            :current-page="page.page"
+            :page-size="page.size"
+            :total="page.total"
+            layout="prev, pager, next"
+            @current-change="changePage"
+          />
+        </el-row>
+      </el-card>
     </div>
+    <!-- 放置组件弹层 -->
+    <add-employee :show-dialog.sync="showDialog" />
   </div>
 </template>
 
 <script>
+import { getEmployeeList, delEmployee } from '@/api/employees'
+import EmployeeEnum from '@/api/constant/employees'
+import AddEmployee from './components/add-employee'
+import { formatDate } from '@/filters'
 export default {
-
+  components: { AddEmployee },
+  data() {
+    return {
+      list: [], // 接数据的
+      page: {
+        page: 1, // 当前页码
+        size: 10,
+        total: 0 // 总数
+      },
+      loading: false, // 显示遮罩层
+      showDialog: false // 默认关闭弹层
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      this.loading = true
+      const { total, rows } = await getEmployeeList(this.page)
+      this.page.total = total
+      this.list = rows
+      this.loading = false
+    },
+    changePage(newPage) {
+      this.page.page = newPage
+      this.getEmployeeList()
+    },
+    // 格式化聘用形式
+    formatEmployment(row, column, cellValue, index) {
+      // 找 1所对应的值
+      const obj = EmployeeEnum.hireType.find(item => item.id === cellValue)
+      return obj ? obj.value : '未知'
+    },
+    // 删除员工
+    async deleteEmployee(id) {
+      try {
+        await this.$confirm('您确定删除该员工吗')
+        await delEmployee(id)
+        this.getEmployeeList()
+        this.$message.success('删除员工成功')
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    // 导出excel数据
+    exportData() {
+      const headers = {
+        '姓名': 'username',
+        '手机号': 'mobile',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+      // 导出excel
+      import('@/vendor/Export2Excel').then(async excel => {
+        //  excel是引入文件的导出对象
+        // 导出  header从哪里来
+        // data从哪里来
+        // 现在没有一个接口获取所有的数据
+        // 获取员工的接口 页码 每页条数    100   1 10000
+        const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        const data = this.formatJson(headers, rows) // 返回的data就是 要导出的结构
+        const multiHeader = [['姓名', '主要信息', '', '', '', '', '部门']]
+        const merges = ['A1:A2', 'B1:F1', 'G1:G2']
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data,
+          filename: '员工资料表',
+          multiHeader, // 复杂表头
+          merges, // 合并选项
+          autoWidth: true,
+          bookType: 'xlsx'
+        })
+      })
+    },
+    // 将表头数据和数据进行对应
+    // [{}]  =>   [[]]
+    formatJson(headers, rows) {
+      return rows.map(item => {
+        // item是一个对象  { mobile: 132111,username: '张三'  }
+        // ["手机号", "姓名", "入职日期" 。。]
+        return Object.keys(headers).map(key => {
+          // 需要判断 字段
+          if (headers[key] === 'timeOfEntry' || headers[key] === 'correctionTime') {
+            // 格式化日期
+            return formatDate(item[headers[key]])
+          } else if (headers[key] === 'formOfEmployment') {
+            const obj = EmployeeEnum.hireType.find(obj => obj.id === item[headers[key]])
+            return obj ? obj.value : '未知'
+          }
+          return item[headers[key]]
+        })
+        // ["132", '张三’， ‘’，‘’，‘’d]
+      })
+      // return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
+      // 需要处理时间格式问题
+    }
+  }
 }
 </script>
 
